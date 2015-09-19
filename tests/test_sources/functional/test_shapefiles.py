6de8a3e2f97b4774ad31a964e80132d0d79f5616
nmb10 <nmb10@dhcppc2.home>
2015-09-19 12:59:32 +0300

    Functional test for all geo sources added. #2.

--- tests/test_sources/functional/test_shapefiles.py
+++ tests/test_sources/functional/test_shapefiles.py
@@ -4,12 +4,15 @@ from ambry_sources import get_source
 from ambry_sources.mpf import MPRowsFile
 
 from fs.opener import fsopendir
+import pytest
+
 from tests import TestBase
 
 
 class Test(TestBase):
     """ shapefiles (*.shp) accessor tests. """
 
+    @pytest.mark.slow
     def test_highways(self):
         # FIXME: Optimize to use local file instead of downloading it all the time.
         cache_fs = fsopendir(self.setup_temp_dir())
@@ -53,3 +56,28 @@ class Test(TestBase):
         self.assertEqual(len(first_row), 68)
         self.assertEqual(first_row['id'], 0)
         self.assertIn('LINESTRING', first_row['geometry'])
+
+    @pytest.mark.slow
+    def test_all(self):
+        # This is very heavey test, so it is disabled. Run it by hand.
+        cache_fs = fsopendir(self.setup_temp_dir())
+
+        sources = self.load_sources(file_name='geo_sources.csv')
+        for name, spec in sources.iteritems():
+            if name == 'highways':
+                # it is already tested. Skip.
+                continue
+
+            source = get_source(spec, cache_fs)
+
+            # now check its load to MPRows
+            mpr = MPRowsFile(cache_fs, spec.name).load_rows(source)
+
+            # Are columns recognized properly?
+            columns = [x['name'] for x in mpr.schema]
+            self.assertIn('id', columns)
+            self.assertIn('geometry', columns)
+
+            # Is first row valid?
+            first_row = next(iter(mpr.reader))
+            self.assertEqual(len(columns), len(first_row))

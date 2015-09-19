ee161439a7f0c97ceeeb29d194960a934abfe63f
nmb10 <nmb10@dhcppc2.home>
2015-09-19 12:37:04 +0300

    Upper case extension filesystem creation fixed. #6.

---
+++ tests/test_sources/functional/regression/test_issue_5.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+from ambry_sources import get_source
+
+from fs.zipfs import ZipFS
+from fs.opener import fsopendir
+
+from tests import TestBase
+
+# Tests https://github.com/CivicKnowledge/ambry_sources/issues/6 issue.
+
+
+class Test(TestBase):
+    """ shapefiles (*.shp) accessor tests. """
+
+    def test_created_source_has_zip_filesystem(self):
+        # FIXME: Optimize to use local file instead of downloading it all the time.
+        cache_fs = fsopendir(self.setup_temp_dir())
+        sources = self.load_sources(file_name='geo_sources.csv')
+        spec = sources['community_plan']
+        source = get_source(spec, cache_fs)
+        self.assertIsInstance(source._fstor._fs, ZipFS)
